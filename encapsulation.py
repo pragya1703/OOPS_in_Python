18a3fb0ffc13a11a4094bfd7a8b7f4719da1be36
“Pragya <pragya.vedita@gmail.com>
2021-02-16 13:48:10 +0530

    Updating encapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -4,27 +4,30 @@ It restrict access to methods and variables. This prevents data from direct modi
 """
 
 class Credential:
-    def __init__(self):
+    def __init__(self, name):
         #We denote private attributes _ and __ as prefix
         self.__password = "Avtkl"
+        self.name = name
     
     def showPassword(self):
-        print("Your current password is {}".format(self.__password))
+        print("Hi {} Your current password is {}".format(self.name, self.__password))
     
-    def updatePassword(self, password):
+    def updatePassword(self, password, name):
         self.__password = password
+        self.name = name
 
 #Used __init__() method to store the password
-user = Credential()
+user = Credential("Person1")
 user.showPassword()
 #Output: Your current password is Avtkl
 
 #tried to update password using updatePassword
-user.updatePassword("JKJKJK")
+user.updatePassword("JKJKJK", "Person2")
 user.showPassword()
 #Output: Your current password is JKJKJK
 
-#Can't change it because Python treats the __password as private attributes.
+#Can't change it because Python treats  __password as private attributes.
 user.__password = "jhjsd"
+user.name = "Person3"
 user.showPassword()
 #Output: Your current password is JKJKJK
